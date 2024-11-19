06c3f181b7f6af5f8b092b84858ded0293feac30
Kevinjomin <122532032+Kevinjomin@users.noreply.github.com>
2024-11-19 14:57:13 +0700

    added player animation

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,14 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundLayer; // Layer for ground detection
 
     private Rigidbody2D rb;
+    private Animator animator;
     private bool isGrounded;
+    private bool isFacingRight = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -22,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
         // Handle horizontal movement
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        SetAnimation(moveInput); //flip player sprite
 
         // Check if the player is grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
@@ -30,6 +35,28 @@ public class PlayerMovement : MonoBehaviour
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void SetAnimation(float moveInput)
+    {
+        if(Mathf.Abs(moveInput) < 0.1f)
+        {
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            animator.SetBool("isRunning", true);
+
+            //flip sprite
+            if ((moveInput < 0f && isFacingRight) || (moveInput > 0f && !isFacingRight))
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                Debug.Log("Flip");
+                transform.localScale = scale;
+            }
         }
     }
 }
